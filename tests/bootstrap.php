42a3c8062bdb21abd3f1217c5888dd152876d50f
Rachman Chavik <rchavik@xintesa.com>
2014-06-02 22:44:50 +0700

    Remove core configuration from test configuration
    - phpunit.xml: Removing DatabaseSuite from cakephp
    - Use our own TestPlugin

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -34,7 +34,7 @@ define('CORE_PATH', CAKE_CORE_INCLUDE_PATH . DS);
 define('CAKE', CORE_PATH . 'src' . DS);
 define('CORE_TESTS', CORE_PATH . 'tests' . DS);
 define('CORE_TEST_CASES', CORE_TESTS . 'TestCase');
-define('TEST_APP', CORE_TESTS . 'test_app' . DS);
+define('TEST_APP', ROOT . DS . 'tests' . DS . 'test_app' . DS);
 define('LOG_ERROR', LOG_ERR);
 
 // Point app constants to the test app.
