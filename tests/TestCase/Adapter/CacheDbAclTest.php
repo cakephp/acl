2fc2c3eb11f6b33c670b8abc0eab998db8075d62
Walther Lalk <walther@octoplus.co.za>
2014-12-03 10:34:00 +0200

    Fix documentation blocks

--- tests/TestCase/Adapter/CacheDbAclTest.php
+++ tests/TestCase/Adapter/CacheDbAclTest.php
@@ -1,11 +1,16 @@
 <?php
 /**
- * Project: hesa-mbit.
- * User: walther
- * Date: 2014/11/25
- * Time: 8:59 AM
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
-
 namespace Acl\Test\TestCase\Adapter;
 
 use Acl\Adapter\CachedDbAcl;
@@ -45,6 +50,10 @@ class CachedDbAclTwoTest extends CachedDbAcl {
 
 }
 
+/**
+ * Test case for AclComponent using the CachedDbAcl implementation.
+ *
+ */
 class CacheDbAclTest extends TestCase {
 
 /**
