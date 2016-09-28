a58302992e43af60e2fc6a2efa14e3e0ea15e2fd
shahonseven <shahonseven@gmail.com>
2016-09-28 23:44:55 +0800

    create acl helper stub

---
+++ src/View/Helper/AclHelper.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\View\Helper;
+
+class AclHelper extends Helper
+{
+    public function __construct(View $View, array $config = []) {}
+    
+    public function link($title, $url = null, array $options = []) {}
+    
+    public function postLink($title, $url = null, array $options = []) {}
+}
