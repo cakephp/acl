d91ec52f24fea2759359cfb175b9c58b6610e5b7
shahonseven <shahonseven@gmail.com>
2016-09-29 21:07:03 +0800

    test stub

---
+++ tests/TestCase/View/Helper/AclHelperTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Acl\Test\TestCase\View\Helper;
+
+use Acl\View\Helper\AclHelper;
+use Cake\Core\Configure;
+use Cake\Network\Request;
+use Cake\Network\Response;
+use Cake\TestSuite\IntegrationTestCase;
+use Cake\View\View;
+
+/**
+ * Class AclHelperTest
+ *
+ */
+class AclHelperTest extends IntegrationTestCase
+{
+    /**
+     * setUp
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        
+    }
+
+    /**
+     * test failure
+     *
+     * @return void
+     */
+    public function testFailure()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * test success
+     *
+     * @return void
+     */
+    public function testSuccess()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}
