a2ee73a6a70c8e7f82d6c39bcd76fbdea2193171
shahonseven <shahonseven@gmail.com>
2016-09-29 21:04:40 +0800

    add comments

--- src/View/Helper/AclHelper.php
+++ src/View/Helper/AclHelper.php
@@ -10,18 +10,38 @@ use Cake\Network\Request;
 use Cake\Routing\Router;
 use Cake\View\Helper;
 
+/**
+ * Acl helper library.
+ *
+ * Automatic generation of links based on ACL.
+ *
+ * @property FormHelper $Form
+ */
 class AclHelper extends Helper
 {
+    /**
+     * List of helpers used by this helper
+     *
+     * @var array
+     */
     public $helpers = ['Form'];
 
+    /**
+     * Default config for the helper.
+     *
+     * @var array
+     */
     protected $_defaultConfig = [
         'userModel' => 'Users'
     ];
 
-    public function __construct(View $View, array $config = []) {
-        parent::__construct($View, $config + $defaults);
-    }
-
+    /**
+     *  Check if the current user have access to the aco
+     *
+     * @param string|array $url
+     *
+     * @return bool
+     */
     public function _check($url)
     {
         $registry  = new ComponentRegistry();
@@ -33,6 +53,11 @@ class AclHelper extends Helper
         return $Acl->check($user, $authorize->action($request));
     }
 
+    /**
+     * Creates an HTML link.
+     *
+     * @link http://book.cakephp.org/3.0/en/views/helpers/html.html#creating-links
+     */
     public function link($title, $url = null, array $options = [])
     {
         if (!$this->_check($url)) {
@@ -41,6 +66,12 @@ class AclHelper extends Helper
         return $this->Form->Html->link($title, $url, $options);
     }
 
+    /**
+     * Creates an HTML link, but access the URL using the method you specify
+     * (defaults to POST). Requires javascript to be enabled in browser.
+     *
+     * @link http://book.cakephp.org/3.0/en/views/helpers/form.html#creating-standalone-buttons-and-post-links
+     */
     public function postLink($title, $url = null, array $options = [])
     {
         if (!$this->_check($url)) {
