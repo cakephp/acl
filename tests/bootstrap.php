06ee7d76257b1ada911cb74d217640dfa014be11
Rachman Chavik <rchavik@xintesa.com>
2017-12-20 10:24:16 +0700

    CS Fix

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -139,7 +139,8 @@ if (class_exists('Carbon\Carbon')) {
 }
 
 if (class_exists('PHPUnit_Runner_Version')) {
-    class_alias('PHPUnit_Framework_TestResult', 'PHPUnit\Framework\TestResult');    class_alias('PHPUnit_Framework_Error', 'PHPUnit\Framework\Error\Error');
+    class_alias('PHPUnit_Framework_TestResult', 'PHPUnit\Framework\TestResult');
+    class_alias('PHPUnit_Framework_Error', 'PHPUnit\Framework\Error\Error');
     class_alias('PHPUnit_Framework_Error_Warning', 'PHPUnit\Framework\Error\Warning');
     class_alias('PHPUnit_Framework_Error_Notice', 'PHPUnit\Framework\Error\Notice');
     class_alias('PHPUnit_Framework_ExpectationFailedException', 'PHPUnit\Framework\ExpectationFailedException');
