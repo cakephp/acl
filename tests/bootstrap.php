15837cd6d4865a218d68377e48b0d711cf903d55
Rachman Chavik <rchavik@xintesa.com>
2017-12-20 10:00:50 +0700

    Fix tests for php 5.6

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -137,3 +137,10 @@ if (class_exists('Carbon\Carbon')) {
     Cake\Chronos\Date::setTestNow(Cake\Chronos\Date::now());
     Cake\Chronos\MutableDate::setTestNow(Cake\Chronos\MutableDate::now());
 }
+
+if (class_exists('PHPUnit_Runner_Version')) {
+    class_alias('PHPUnit_Framework_TestResult', 'PHPUnit\Framework\TestResult');    class_alias('PHPUnit_Framework_Error', 'PHPUnit\Framework\Error\Error');
+    class_alias('PHPUnit_Framework_Error_Warning', 'PHPUnit\Framework\Error\Warning');
+    class_alias('PHPUnit_Framework_Error_Notice', 'PHPUnit\Framework\Error\Notice');
+    class_alias('PHPUnit_Framework_ExpectationFailedException', 'PHPUnit\Framework\ExpectationFailedException');
+}
