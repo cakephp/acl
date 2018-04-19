5df2acfd2779a1fff842e206f0bcfe02705d28c3
tsmsogn <tsmsogn@gmail.com>
2018-04-19 16:54:28 +0900

    Remove statements never called

--- src/Shell/AclShell.php
+++ src/Shell/AclShell.php
@@ -83,14 +83,6 @@ class AclShell extends Shell
         }
 
         if ($this->command) {
-            if (Configure::check('Datasource') === null) {
-                $this->out(__d('cake_acl', 'Your database configuration was not found. Take a moment to create one.'));
-                $this->args = null;
-                $this->DbConfig->execute();
-
-                return;
-            }
-
             try {
                 TableRegistry::getTableLocator()->get('Aros')->getSchema();
                 TableRegistry::getTableLocator()->remove('Aros');
