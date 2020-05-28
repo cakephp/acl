1469ee294ea4d3df98fa29b4408302d5e387aaff
nook24 <info@nook24.eu>
2020-05-28 14:48:29 +0200

    Resolve 'Array and string offset access syntax with curly braces is deprecated' for php 7.4

--- src/Model/Table/PermissionsTable.php
+++ src/Model/Table/PermissionsTable.php
@@ -182,14 +182,14 @@ class PermissionsTable extends AclNodesTable
             $save = array_combine($permKeys, array_pad([], count($permKeys), $value));
         } else {
             if (!is_array($actions)) {
-                if ($actions{0} !== '_') {
+                if (substr($actions, 0, 1) !== '_') {
                     $actions = ['_' . $actions];
                 } else {
                     $actions = [$actions];
                 }
             }
             foreach ($actions as $action) {
-                if ($action{0} !== '_') {
+                if (substr($action, 0, 1) !== '_') {
                     $action = '_' . $action;
                 }
                 if (!in_array($action, $permKeys, true)) {
@@ -268,3 +268,4 @@ class PermissionsTable extends AclNodesTable
         return $newKeys;
     }
 }
+
